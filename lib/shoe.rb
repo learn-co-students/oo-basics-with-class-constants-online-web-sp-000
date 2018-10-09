8bf5839681b8f68df657cc9b5a6daec48e3c640e
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-09 19:45:41 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   BRANDS = []
+  rubbish = []
+
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
+    if BRANDS.include?(brand) == false
+      BRANDS << brand
+    end
   end
 
   def cobble
