35831866355a4ca4330e0ee7285247458bf54138
Benjamin M Lancaster <bmlancaster07@gmail.com>
2019-01-02 23:27:45 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,18 +6,14 @@ BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
-  end
-  
-  def initialize(brand)
-    @brand = brand
-    if !(BRANDS.include?(@brand))
-      BRANDS << @brand
-    end
   end
 
 end
