7940ed51c4ad40500e1a123a352a430b58a16f04
Connor Cowan <ccowan003@comcast.net>
2019-06-22 01:23:43 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,14 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  GENRES = []
+  
   def initialize(brand)
     @brand = brand
   end
 
   def cobble
-    self.condition = "new"
+    self.condition = "new"                #self refers to our instance of a class here
     puts "Your shoe is as good as new!"
   end
 
