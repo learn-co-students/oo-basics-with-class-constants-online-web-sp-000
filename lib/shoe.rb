36551c06e8d1100b12a592f2f8f43cbeca681173
Connor Cowan <ccowan003@comcast.net>
2019-06-22 01:48:47 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,23 +1,16 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  #attr_reader :brand
+  attr_reader :brand
 
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand=(brand)
-    @brand = brand
-    if BRAND == "new"
-      BRAND << brand
-    
+    BRANDS << brand unless BRANDS.include?(brand)         #add BRANDS to array unless its already included. We want unique brands
   end
 
   def cobble
-    self.condition = "new"                #self refers to our instance of a class here
+    self.condition = "new"                        #self refers to our instance of a class here
     puts "Your shoe is as good as new!"
   end
-
 end
