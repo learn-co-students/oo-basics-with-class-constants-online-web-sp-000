a7abc6b43b99754a6066a98dec8834962fdbdc16
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-11 19:24:14 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,9 @@ class Shoe
   
   BRANDS = []
   
-  
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.any? { |b| b == brand }
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
 
@@ -17,6 +16,4 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-  
 end
