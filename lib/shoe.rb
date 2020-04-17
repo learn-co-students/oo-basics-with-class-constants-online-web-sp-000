f503e6474431c4ae97875b2dc70d8a696fd1633a
Elliot Blanchard <public@elliotblanchard.com>
2020-04-17 00:13:06 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,19 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include? brand
+      BRANDS << brand
+    end
+  end
+
+  # create the writer for brand and add the logic for the class constant
+  def brand=(brand)
+    @brand = brand
+    #BRANDS << brand
   end
 
   def cobble
