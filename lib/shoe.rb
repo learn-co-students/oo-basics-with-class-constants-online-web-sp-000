61cd906ae6917aedfb70f022f4fb9d0be611f8aa
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-18 03:38:58 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,17 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
+  
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.length == 0
+      BRANDS << @brand
+    elsif BRANDS[BRANDS.length-1] != @brand
+      BRANDS << @brand
+    end
   end
 
   def cobble
