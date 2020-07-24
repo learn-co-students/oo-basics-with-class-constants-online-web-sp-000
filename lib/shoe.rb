4f39512bc9567bdcd08aeea4d79debc6fb8f6539
Nick Lanese <nlanese21@gmail.com>
2020-07-24 05:20:46 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = ["Uggs" , "Rainbow" , "Nike"]
+
   def initialize(brand)
     @brand = brand
   end
