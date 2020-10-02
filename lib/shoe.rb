2b66ab1e212dbaeb0984605053fe8335933e8d13
Alan Tang <giabao_tang@yahoo.com>
2020-10-02 06:25:56 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,17 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+   if BRANDS == []
+     BRANDS << brand 
+    
+   elsif BRANDS.all? { |existing_brand| brand != existing_brand}
+       BRANDS << brand 
+   end    
   end
 
   def cobble
