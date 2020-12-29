85189b83b8dac6d624b721af60fec83d039850ee
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-29 00:40:14 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,23 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
+  attr_accessor :color, :material, :condition
+  attr_reader :brand, :size 
+  BRANDS = []
+  
   def initialize(brand)
-    @brand = brand
-  end
+    @brand= brand 
+     BRANDS << !BRANDS.includes? brand 
+   end 
+     
+     def compare
+       if @brand.!includes BRANDS
+         BRANDS << brand 
+      end
+    end 
 
+  def size
+    @size= 3
+  end 
+  
   def cobble
     self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-
-end
+ 
