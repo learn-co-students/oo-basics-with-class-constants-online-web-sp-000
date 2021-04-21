5e4bbbcc01f5b6e8b49bb4e27868938573f138fe
Harrison Price <hprice518@gmail.com>
2021-04-21 17:10:36 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,21 +1,22 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand, :brands
+  attr_reader :brand, :new_brand
 
-  BRANDS = []
   
   def initialize(brand)
     @brand = brand
   end
 
+  BRANDS = []
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
-  def brands= (brands)
-    @brands = brands
-    BRANDS << brands
-  end
 
+  def new_brand= (new_brand)
+      @new_brand = new_brand
+      BRANDS << new_brand
+  end
+  
 end
